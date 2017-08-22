8e29034066e07cfefe5358c653f50144451739c6
KENTARO SATA <s.ken0331@gmail.com>
2017-08-22 15:05:01 +0900

    add layout of LightCalendarView

---
+++ app/src/main/java/com/example/ken/calendar/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.ken.calendar
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import jp.co.recruit_mp.android.lightcalendarview.LightCalendarView
+
+class MainActivity : AppCompatActivity() {
+
+    val calendarView: LightCalendarView by lazy { findViewById(R.id.calendar_view) as LightCalendarView }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
